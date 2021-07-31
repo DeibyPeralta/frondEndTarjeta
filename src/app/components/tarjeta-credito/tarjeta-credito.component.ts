ee287a9b5020d79cfe78545de73bc6a6a9eb6b42
deiby <@gmail.com>
2021-07-31 15:18:03 -0500

    validations create

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -8,15 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class TarjetaCreditoComponent implements OnInit {
 
   listTarjetas: any[] = [
-    { titulo: 'juan peres', numeroTarjeta: 1223344512344512,  fechaExpliracion: '11/12', cvv: '123' },
-    { titulo: 'juan fasd', numeroTarjeta: 1223344512344512,  fechaExpliracion: '23/12', cvv: '234' },
-    { titulo: 'asdfsd peres', numeroTarjeta: 1223344512344512,  fechaExpliracion: '31/12', cvv: '426' },
-    { titulo: 'peres anita', numeroTarjeta: 1223344512344512,  fechaExpliracion: '25/12', cvv: '623' }
+    { titular: 'juan peres', numeroTarjeta: 1223344512344512,  fechaExpliracion: '11/12', cvv: '123' },
+    { titular: 'juan fasd', numeroTarjeta: 1223344512344512,  fechaExpliracion: '23/12', cvv: '234' },
+    { titular: 'asdfsd peres', numeroTarjeta: 1223344512344512,  fechaExpliracion: '31/12', cvv: '426' },
+    { titular: 'peres anita', numeroTarjeta: 1223344512344512,  fechaExpliracion: '25/12', cvv: '623' }
   ];
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta:['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaExpliracion: ['', [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+      cvv: ['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  agregar(){
+    const tarjetas: any ={
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpliracion: this.form.get('fechaExpliracion')?.value,
+      cvv: this.form.get('cvv')?.value
+    }
+
+    this.listTarjetas.push(tarjetas);
+
+    this.form.reset();
+  }
 }
